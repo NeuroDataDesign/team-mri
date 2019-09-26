955b5ad8be18da3c9569b4cfebfb221ce6b0299b
jialin kang <kjlyayst@163.com>
2019-09-26 19:53:51 -0400

    modify merge_dicts

--- Jialin_Kang/week3/test_merge_dicts.py
+++ Jialin_Kang/week3/test_merge_dicts.py
@@ -1,10 +1,14 @@
-def merge_dicts(x, y):
-    z = x.copy()
-    z.update(y)
-    return z
+import ndmg
+
+
+
+# def merge_dicts(x, y):
+#     z = x.copy()
+#     z.update(y)
+#     return z
 
 def test_merge_dicts():
     x = {'name':'Jack', 'age':'20'}
     y = {'sex':'man'}
-    z = merge_dicts(x, y)
+    z = ndmg.utils.bids_utils.merge_dicts(x, y)
     assert z == {'name':'Jack', 'age':'20', 'sex':'man'}
