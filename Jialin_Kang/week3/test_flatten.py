1471ad03156e01251c870a910efe851b69f45d9d
jialin kang <kjlyayst@163.com>
2019-09-26 19:34:50 -0400

    modify flatten

--- Jialin_Kang/week3/test_flatten.py
+++ Jialin_Kang/week3/test_flatten.py
@@ -1,10 +1,14 @@
-def flatten(current, result=[]):
-    if isinstance(current, dict):
-        for key in current:
-            flatten(current[key], result)
-    else:
-        result.append(current)
-    return result
+# def flatten(current, result=[]):
+#     if isinstance(current, dict):
+#         for key in current:
+#             flatten(current[key], result)
+#     else:
+#         result.append(current)
+#     return result
+
+import ndmg
+
+ndmg.utils.bids_utils.flatten
 
 def test_flatten():
     current = {'name':'liming', 'age':'20'}
