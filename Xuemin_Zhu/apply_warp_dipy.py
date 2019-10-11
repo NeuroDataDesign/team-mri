7610472725bbdaf04a5153a04dc7a3cffcd7c46c
XueminZhu-Charmaine <54967460+XueminZhu-Charmaine@users.noreply.github.com>
2019-10-11 09:24:10 -0400

    Update apply_warp_dipy.py

--- Xuemin_Zhu/apply_warp_dipy.py
+++ Xuemin_Zhu/apply_warp_dipy.py
@@ -1,5 +1,5 @@
 #This is a pure python function for apply_warp
-import dipy
+
 import warnings
 
 warnings.simplefilter("ignore")
@@ -9,26 +9,86 @@ import numpy as np
 import nilearn.image as nl
 import os
 import os.path as op
-import matplotlib.image as mpimg
+import nilearn.image as nl
 
-from ndmg.utils import reg_utils as rgu
 
-"""Applies a warp from the structural to reference space in a single step using information about
-    the structural -> ref mapping as well as the functional to structural mapping.
-    
-    Parameters
-    ----------
-    in_nii : 
-        the reference image to be aligned to
-    nonlin_nii: 
-        the input image to be aligned
-    out_nii : 
-        the resulting warped output image
-    affine_mat : 
-        the warp coefficent file to go from inp -> ref
-"""
-dipy.external.fsl.apply_warp(in_nii, affine_mat, nonlin_nii, out_nii)
-in_nii = mping.imread(ref)
-affine = mping.imread(warp)
-nonlin_nii = mping.imread(inp)
-out_nii=mping.imread(out)
+from __future__ import (print_function, division, unicode_literals,
+                        absolute_import)
+from builtins import open
+
+import os.path as op
+import re
+
+from ... import logging
+from .base import ElastixBaseInputSpec
+from ..base import CommandLine, TraitedSpec, File, traits, InputMultiPath
+
+
+
+class ApplyWarpOutputSpec(TraitedSpec):
+    warped_file = File(desc='input moving image warped to fixed image')
+
+
+class ApplyWarp(CommandLine):
+    """
+    Use ``transformix`` to apply a transform on an input image.
+    The transform is specified in the transform-parameter file.
+    """
+
+    _cmd = 'transformix'
+    input_spec = ApplyWarpInputSpec
+    output_spec = ApplyWarpOutputSpec
+
+    def _list_outputs(self):
+        outputs = self._outputs().get()
+        out_dir = op.abspath(self.inputs.output_path)
+        outputs['warped_file'] = op.join(out_dir, 'result.nii.gz')
+        return outputs
+
+
+class AnalyzeWarpInputSpec(ApplyWarpInputSpec):
+    points = traits.Enum(
+        'all',
+        usedefault=True,
+        position=0,
+        argstr='-def %s',
+        desc='transform all points from the input-image, which effectively'
+             ' generates a deformation field.')
+    jac = traits.Enum(
+        'all',
+        usedefault=True,
+        argstr='-jac %s',
+        desc='generate an image with the determinant of the spatial Jacobian')
+    jacmat = traits.Enum(
+        'all',
+        usedefault=True,
+        argstr='-jacmat %s',
+        desc='generate an image with the spatial Jacobian matrix at each voxel')
+    moving_image = File(
+        exists=True,
+        argstr='-in %s',
+        desc='input image to deform (not used)')
+
+class AnalyzeWarpOutputSpec(TraitedSpec):
+    disp_field = File(desc='displacements field')
+    jacdet_map = File(desc='det(Jacobian) map')
+    jacmat_map = File(desc='Jacobian matrix map')
+
+
+class AnalyzeWarp(ApplyWarp):
+    """
+    Use transformix to get details from the input transform (generate
+    the corresponding deformation field, generate the determinant of the
+    Jacobian map or the Jacobian map itself)
+    """
+
+    input_spec = AnalyzeWarpInputSpec
+    output_spec = AnalyzeWarpOutputSpec
+
+    def _list_outputs(self):
+        outputs = self._outputs().get()
+        out_dir = op.abspath(self.inputs.output_path)
+        outputs['disp_field'] = op.join(out_dir, 'deformationField.nii.gz')
+        outputs['jacdet_map'] = op.join(out_dir, 'spatialJacobian.nii.gz')
+        outputs['jacmat_map'] = op.join(out_dir, 'fullSpatialJacobian.nii.gz')
+        return outputs
